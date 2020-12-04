96b9df3e3210c2d1ea0e8338b8d262a1f849fb33
Martin Macharia <mosmartin5@gmail.com>
2020-12-04 09:06:08 +0100

    Add middleware
    Add logging, security and cors middleware.

---
+++ src/middleware/logger.js
@@ -0,0 +1,27 @@
+const { createLogger, format, transports } = require('winston');
+require('winston-daily-rotate-file');
+const { combine, timestamp, label, prettyPrint } = format;
+
+const transport = new transports.DailyRotateFile({
+  filename: 'freachly-server-%DATE%.log',
+  dirname: './logs',
+  // frequency: '1m',
+  datePattern: 'YYYY-MM-DD',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '14d',
+  auditFile: 'log-audit.json',
+});
+
+transport.on('rotate', function (oldFilename, newFilename) {
+  // do something fun
+});
+
+const logger = createLogger({
+  level: 'silly',
+  format: combine(label({ label: 'log' }), timestamp(), prettyPrint()),
+  defaultMeta: { service: 'freachly-martin-challenge-server' },
+  transports: [transport],
+});
+
+module.exports = logger;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,25 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const debug = require('debug')('app:server');
 const userRoute = require('../src/routes/user.routes');
+const log = require('./middleware/logger');
 
 // instantiate the app
 const app = express();
+
+// secure server by setting various HTTP headers.
+app.use(helmet());
+
+// handle cors errors
+app.use(cors());
+
+// dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // mount routes
 app.use('/api/v1/users', userRoute);
@@ -13,4 +28,5 @@ const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   debug(`🚀 Server listening in ${process.env.NODE_ENV} on port ${PORT}`);
+  log.info(`🚀 Server listening in ${process.env.NODE_ENV} on port ${PORT}`);
 });
